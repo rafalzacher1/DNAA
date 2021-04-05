9aec6036dfca8fa4a3eb1477f92f7e0926438c45
rafalzacher1 <46526911+rafalzacher1@users.noreply.github.com>
2021-04-05 13:25:11 +0100

    Created DB connection file.

--- libs/db.js
+++ libs/db.js
@@ -1,3 +1,10 @@
-/**
- * http://usejsdoc.org/
- */
+const {Client} = require('pg')
+
+const client = new Client({
+	user: 'postgre',
+	host: 'localhost',
+	database: 'DNA',
+	password: 'Password-123'
+})
+
+client.connect()
