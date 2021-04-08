cbd2875e49bfc10a1e3d5b64554392a18cbd6a8d
rafalzacher1 <46526911+rafalzacher1@users.noreply.github.com>
2021-04-08 13:18:52 +0100

    Added functionality for a user to send a message to DNA.

---
+++ routes/messageUs.js
@@ -0,0 +1,33 @@
+const routes = require('express').Router();
+const nodemailer = require('nodemailer');
+
+routes.post('/', (request, result) => {
+	let {name, email, message} = request.body;
+	
+	let transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: 'rafalzacher99@gmail.com', // Your email.
+			pass: '$rLspefo7&8R8n' // Your email password.
+		}
+	});
+	
+	let mailOptions = {
+		from: email,
+		to: 'rafalzacher99@gmail.com', // Your email.
+		subject: 'Website contact us page message.',
+		text: message + '. From ' + name
+	};
+	
+	transporter.sendMail(mailOptions, (error, info) => {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log('Email sent: ' + info.reponse);
+		}
+	});
+	
+	result.redirect('back');
+});
+
+module.exports = routes;
