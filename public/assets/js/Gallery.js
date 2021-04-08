0ed94687a7d125931d21eb80530158d2858059a9
rafalzacher1 <46526911+rafalzacher1@users.noreply.github.com>
2021-04-08 13:17:05 +0100

    Updated the blog posts to direct to the correct pages

--- public/assets/js/Gallery.js
+++ public/assets/js/Gallery.js
@@ -3,16 +3,16 @@ function ChangeGallery(galleryName) {
     let e2019_2020 = document.getElementById("year2019-2020");
     
     if (galleryName == "2020-2021") {
+        e2020_2021.style.visibility = "visible";
         e2020_2021.style.display = "block";
-        e2020_2021.classList += " active";
+        e2019_2020.style.visibility = "hidden";
         e2019_2020.style.display = "none";
-        e2019_2020.classList.remove("active")
     }
     
     if (galleryName == "2019-2020") {
+        e2020_2021.style.visibility = "hidden";
         e2020_2021.style.display = "none";
-        e2020_2021.classList.remove("active");
+        e2019_2020.style.visibility = "visible";
         e2019_2020.style.display = "block";
-        e2019_2020.classList += " active";
     }
 };
